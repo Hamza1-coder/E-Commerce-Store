fa22e5f09f591494c881c40d38966e956f96729e
Ameer Hamza <61383954+Hamza1-coder@users.noreply.github.com>
2024-04-03 01:05:20 +0500

    implement form validation with zod and configure prisma and supabase to store data.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - Environment setup (Admin) ✅
 - Clerk Authentication (Admin) ✅
 - Modal components (Admin) ✅
+- Form components with zod validation (Admin) ✅
+- Prisma, Supabase, MySQL setup (Admin) ✅
--- e-commerce/components/modals/store-modal.tsx
+++ e-commerce/components/modals/store-modal.tsx
@@ -1,9 +1,52 @@
 "use client";
+import * as z from "zod";
 import { useStoreModalStore } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModalStore();
+
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // console.log(values);
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/stores", values);
+      toast.success("Store created");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Modal
       title="Create Store"
@@ -11,7 +54,41 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Future Create Store Form
+      <div className="space-y-4 py-2 pb-2">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="E-Commerce"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button
+                disabled={loading}
+                variant="outline"
+                onClick={storeModal.onClose}
+              >
+                Cancel
+              </Button>
+              <Button disabled={loading} type="submit">
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </Modal>
   );
 };
